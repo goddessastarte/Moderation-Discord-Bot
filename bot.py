93cca0e3eb8ff4fc8b48da04e67c75394274c6be
Goddess <secretgoddess6666@gmail.com>
2025-03-20 23:17:00 +0000

    enable all intents and run the bot

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 import os 
 from dotenv import load_dotenv
+import discord 
+from discord.ext import commands 
 
+# load .env variables
 load_dotenv()
-
 discord_token = os.getenv("discord_token")
+
+# enable all intents
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# run the bot
+bot.run(discord_token)
