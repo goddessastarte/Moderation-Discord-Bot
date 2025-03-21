cc70e6e5b2886d4e5080c5efe9fa5885951deb03
Goddess <secretgoddess6666@gmail.com>
2025-03-21 19:47:13 +0000

    add more commands

--- bot.py
+++ bot.py
@@ -24,6 +24,23 @@ async def hello(ctx):
 async def slash_hello(interaction: discord.Interaction):
     await interaction.response.send_message("Haiiii <3 !!!")
 
+# add a /ping command 
+@bot.tree.command(name="ping",description="ping pong")
+async def pingpong(interaction: discord.Interaction):
+    await interaction.response.send_message("pong")
+
+# add a /info command 
+@bot.tree.command(name="info", description="give info about the bot")
+async def infobot(interaction : discord.Interaction):
+    #app_info = bot.application.owner
+    await interaction.response.send_message(f"{bot.application.owner} {bot.user} {bot.activity} {bot.status}")
+
+# add a /say command
+@bot.tree.command(name="say", description="repeats the message the user gives")
+@discord.app_commands.describe(message="The message you want the bot to repeat")
+async def say(interaction: discord.Interaction, message:str):
+    await interaction.response.send_message(message)
+
 # register and update slash commands with discord
 @bot.event
 async def on_ready():
